2723337275e7a71a5c1439787f6704d952359dd6
saja <sajaabdulaziz77@gmail.com>
2021-11-29 20:02:11 +0300

    save

---
+++ test.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<body>
+<?php
+$con=mysqli_connect("localhost","root","","ExploreQassim")
+or die(mysqli_connect_error());
+echo "connect done"."<br>";
+mysqli_query($con,"CREATE TABLE IF NOT EXISTS suggestion(
+  id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY(id),
+  name VARCHAR(30),
+  email VARCHAR(50),
+  message TEXT)")
+  or die(mysqli_connect_error());
+echo "created";
+$query="INSERT INTO suggestion(name,email,message)VALUES(' " .$_POST["username"]." ',' ".$_POST["useremail"]." ', ' ".$_POST["message"]." ')";
+mysqli_query($con,$query)
+or die(mysqli_connect_error());
+echo "inserted";
+
+?>
+</html>
+</html>
